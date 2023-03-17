ec65e099b7c37e0cf5aa9f21ce1289e52bb4a384
Виталий Тютюников <vtyutyunikov@ekonomexpert.ru>
2023-03-17 19:10:06 +0500

    -

--- Renting.Server/Renting/DAL/RentingDbContext.cs
+++ Renting.Server/Renting/DAL/RentingDbContext.cs
@@ -1,9 +1,4 @@
-using System;
-
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-
-using Renting.DAL.Configurations;
 using Renting.DAL.Entities;
 
 namespace Lab9.App.DAL
@@ -21,12 +16,5 @@ namespace Lab9.App.DAL
         public DbSet<Rent> Rents => Set<Rent>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Warehouse> Warehouses => Set<Warehouse>();
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            //builder.ApplyConfiguration(new RentConfiguration());
-
-            base.OnModelCreating(builder);
-        }
     }
 }
